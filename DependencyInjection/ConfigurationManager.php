4fb98d759c5a5e19abcaebc716be1dd7659c9689
Jean Bordat <bordat.jean@gmail.com>
2014-09-21 19:13:18 +0200

    Comments
    Signed-off-by: Jean Bordat <bordat.jean@gmail.com>

--- DependencyInjection/ConfigurationManager.php
+++ DependencyInjection/ConfigurationManager.php
@@ -8,23 +8,39 @@
 
 namespace SpiritDev\Bundle\OAuth2ClientBundle\DependencyInjection;
 
+/**
+ * Class ConfigurationManager
+ * @package SpiritDev\Bundle\OAuth2ClientBundle\DependencyInjection
+ *
+ * @author Jean BORDAT <bordat.jean@gmail.com>
+ * @date    21/09/2014
+ * @time    19:11
+ */
 class ConfigurationManager {
 
     private $templateName = null;
 
+    /**
+     * @param $template Param defined in config.yml
+     *
+     *
+     * @author Jean BORDAT <bordat.jean@gmail.com>
+     * @date    21/09/2014
+     * @time    19:10
+     */
     public function __construct($template) {
         $this->templateName = $template;
     }
 
     /**
-     * @return null
+     * @return String template name
      */
     public function getTemplateName() {
         return $this->templateName;
     }
 
     /**
-     * @param null $templateName
+     * @param String $templateName
      */
     public function setTemplateName($templateName) {
         $this->templateName = $templateName;
